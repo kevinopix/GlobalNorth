b947e07e9aad352a637ce92609d0b3c2f169bb7f
kevinopix <kevinopix@gmail.com>
2023-03-08 16:25:18 -0600

    Forgot your Password Email Config

--- accounts/views/password_reset_email.py
+++ accounts/views/password_reset_email.py
@@ -9,6 +9,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.contrib import messages
+from django.conf import settings
 
 
 def password_reset_request(request):
@@ -23,11 +24,11 @@ def password_reset_request(request):
                     email_template_name = "registration/password_reset_email.txt"
                     c = {
                         "email": user.email,
-                        'domain': '127.0.0.1:8000',
-                        'site_name': 'Website',
+                        'domain': settings.EMAIL_SETUP_DOMAIN,
+                        'site_name': settings.EMAIL_SETUP_SITENAME,
                         "uid": urlsafe_base64_encode(force_bytes(user.pk)),
                         'token': default_token_generator.make_token(user),
-                        'protocol': 'http',
+                        'protocol': settings.EMAIL_SETUP_PROTOCOL,
                     }
                     email = render_to_string(email_template_name, c)
                     try:
