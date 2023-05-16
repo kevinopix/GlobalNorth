d7775b9465186861165ca83b2e41593047e9d5b1
kevinopix <kevinopix@gmail.com>
2023-05-16 13:30:33 -0500

    Clean up

--- Payment/urls.py
+++ Payment/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', ProductListView.as_view(), name='home'),
-    # path('create/', ProductCreateView.as_view(), name='create'),
-    # path('detail/<id>/', ProductDetailView.as_view(), name='detail'),
-    # path('history/', OrderHistoryListView.as_view(), name='history'),
-    # path('api/checkout-session/<id>/', create_checkout_session, name='api_checkout_session'),
-    # path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
-    path('config/', stripe_config),
+    path('config/', stripe_config, name="stripe-config"),
     path('create-checkout-session/<int:pk>/', create_checkout_session, name='create-checkout-session'),
     path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
     path('pay/success/', PaymentSuccessView.as_view(), name='success_stripe_pay'),
