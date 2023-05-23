5c7facb5a0dd3eb55c9e1beba4100c2c3676647a
kevinopix <kevinopix@gmail.com>
2023-05-22 20:19:49 -0500

    Requirements

--- GlobalNorth/settings.py
+++ GlobalNorth/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'GlobalNorth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'theglobalnorth_postgresdb',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
