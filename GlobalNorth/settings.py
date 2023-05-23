a80214358776f6f8bdaa95290cb5c81165ccae0f
kevinopix <kevinopix@gmail.com>
2023-05-22 23:40:57 -0500

    Deploy

--- GlobalNorth/settings.py
+++ GlobalNorth/settings.py
@@ -164,8 +164,10 @@ USE_TZ = True
 #
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+# STATIC_URL = '/static/'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
