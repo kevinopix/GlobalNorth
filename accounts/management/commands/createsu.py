43bc5e1e82eeb04867310092b7a2b58ba421693d
kevinopix <kevinopix@gmail.com>
2023-05-23 12:48:01 -0500

    Deploy

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -6,12 +6,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(email="kevinopixadm@gmail.com").exists():
-            new_user = User()
-            new_user.email = "kevinopixadm@gmail.com"
+            User.objects.create_superuser(
+                username='kevinopixadm@gmail.com',
+                password='pa00037011'
+            )
+            new_user = User.objects.get(email="kevinopixadm@gmail.com")
             new_user.first_name = "Kevin"
             new_user.last_name = "Okome"
-            new_user.password = "pa00037011"
-            new_user.is_staff = True
-            new_user.is_superuser = True
             new_user.save()
             # User.objects.create_superuser("admin", "admin@admin.com", "admin")
