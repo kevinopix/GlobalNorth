23f8d99716ca998fa41269c069f3661fdf034c6c
kevinopix <kevinopix@gmail.com>
2023-05-24 16:41:24 -0500

    Deploy

--- GlobalNorth/settings.py
+++ GlobalNorth/settings.py
@@ -178,22 +178,26 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_URL = 'static/'
-# # STATIC_URL = '/static/'
-#
-# # Add these new lines
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 # STATIC_URL = '/static/'
 # STATIC_URL = '/static/'
 # STATIC_ROOT = 'static'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
