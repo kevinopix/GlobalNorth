e09e6cc8f3a95022b7ebfe43280a9cf721ed8630
kevinopix <kevinopix@gmail.com>
2023-06-09 10:28:06 -0500

    Stripe Config

--- services/models/package_price.py
+++ services/models/package_price.py
@@ -4,7 +4,7 @@ from services.models import Package
 
 class Price(models.Model):
     product = models.ForeignKey(Package, on_delete=models.CASCADE)
-    price = models.DecimalField(decimal_places=2, max_digits=10)
+    price = models.DecimalField(decimal_places=2, max_digits=10, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
